a097b99ce789c5b1ced55b075c909276036c9238
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-13 00:36:55 +0100

    feat: generate user resource

--- hyperlocal-server/src/app.module.ts
+++ hyperlocal-server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+
 @Module({
-  imports: [DatabaseModule]
+  imports: [DatabaseModule, UserModule]
 })
 export class AppModule {}
