b061edd065531151f1f14e8122f32b41b11dcff1
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-13 23:21:20 +0100

    feat: swagger implementation in the main

--- hyperlocal-server/src/main.ts
+++ hyperlocal-server/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  const config = new DocumentBuilder()
+  .setTitle('Hyperlocal Franquias')
+  .setDescription('API de gestão de franqueados e administração de franquias')
+  .setVersion('1.0.0')
+  .addTag('User')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   
   await app.listen(process.env.PORT ?? 3000);
 }
