954d553bcbd64e859f6392bc6c0e8d06cc1a377c
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-13 23:54:12 +0100

    feat: add create user route

--- hyperlocal-server/src/user/user.controller.ts
+++ hyperlocal-server/src/user/user.controller.ts
@@ -3,6 +3,8 @@ import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { HandleException } from 'src/utils/exceptions/exceptionHandler';
+import { IUser } from './entities/user.entity';
 
 @Controller('user')
 @ApiTags('User')
@@ -10,8 +12,12 @@ export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  create(@Body() createUserDto: CreateUserDto): Promise<IUser> {
+  try {
+  return this.userService.create(createUserDto);
+  } catch (error) {
+  HandleException(error);
+  }
   }
 
   @Get()
--- hyperlocal-server/src/user/user.service.ts
+++ hyperlocal-server/src/user/user.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { hash } from 'bcrypt';
+import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return ;
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const userId = uuidv4();
+    const hashedPassword = await hash(createUserDto.password, 10);
+    createUserDto.password = hashedPassword;
+    const user = {...createUserDto, id: userId};
+    const createdUser = await this.userRepository.create(user);
+    return createdUser;
   }
 
   findAll() {
