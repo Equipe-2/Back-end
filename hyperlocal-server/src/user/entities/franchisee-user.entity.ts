8959fdc463ef7a0ba87efba22a1736dd4dcd2927
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-14 14:05:31 +0100

    feat: add franchisee entity

---
+++ hyperlocal-server/src/user/entities/franchisee-user.entity.ts
@@ -0,0 +1,17 @@
+export interface IFranchiseeUser{
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+  franchisee: IFranchisee;
+}
+
+export interface IFranchisee {
+  userId: string;
+  personalEmail: string;
+  cnpj: string;
+  phoneNumber: string;
+  city: string;
+  address: string;
+}
