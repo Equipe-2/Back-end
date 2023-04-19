89ec8b9a861f60cdc32acb35f615750b829e464e
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-19 01:10:13 +0100

    feat: build auth module and add the secret key in .env

--- hyperlocal-server/src/auth/auth.module.ts
+++ hyperlocal-server/src/auth/auth.module.ts
@@ -1,12 +1,23 @@
 import { Module } from "@nestjs/common";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { PassportModule } from "@nestjs/passport";
+import { JwtModule } from "@nestjs/jwt";
+import { AuthStrategy } from "./auth-strategy";
+import { UserModule } from "src/user/user.module";
+import { UserService } from "src/user/user.service";
+import { UserRepository } from "src/user/user.repository";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Module({
-    imports:[],
+    imports:[PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.SECRET_KEY,
+      signOptions: { expiresIn: '24h' },
+    })],
     controllers:[AuthController],
-    providers:[AuthService],
-    exports:[]
+    providers:[AuthService, AuthStrategy, UserService, UserRepository, PrismaService],
+    exports:[AuthStrategy, AuthService]
 })
 export class AuthModule {}
