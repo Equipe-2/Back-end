504e38786eb54e1f368b290ee98d9a19f1e13eb7
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-24 23:22:29 +0100

    feat: generating new route named tier

--- hyperlocal-server/src/app.module.ts
+++ hyperlocal-server/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TierModule } from './tier/tier.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule],
+  imports: [DatabaseModule, UserModule, AuthModule, TierModule],
 })
 export class AppModule {}
