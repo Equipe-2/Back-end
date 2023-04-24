f3dc704fb2e4e7baed887bda95680bf83cac148e
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-24 23:54:53 +0100

    feat: add tier entity

--- hyperlocal-server/src/tier/entities/tier.entity.ts
+++ hyperlocal-server/src/tier/entities/tier.entity.ts
@@ -1,1 +1,8 @@
-export class Tier {}
+export class Tier {
+    id: string;
+    name: string;
+    scoreMin: number; 
+    scoreMax: number;
+    percentageSaas: number;
+    percentageMDR: number;
+}
