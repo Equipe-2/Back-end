2962819cedcaa1d2c72f6ab1c198f85a2d689a34
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-25 23:10:57 +0100

    feat: generating client basic structure

--- hyperlocal-server/src/app.module.ts
+++ hyperlocal-server/src/app.module.ts
@@ -3,8 +3,9 @@ import { DatabaseModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TierModule } from './tier/tier.module';
+import { ClientModule } from './client/client.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule, AuthModule, TierModule],
+  imports: [DatabaseModule, UserModule, AuthModule, TierModule, ClientModule],
 })
 export class AppModule {}
