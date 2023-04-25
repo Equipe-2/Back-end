dec0cf7e6dfb4476a4e23947754e40abf0aeee8b
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-25 23:34:36 +0100

    feat: add client's dtos and enity

--- hyperlocal-server/src/client/dto/create-client.dto.ts
+++ hyperlocal-server/src/client/dto/create-client.dto.ts
@@ -1,1 +1,6 @@
-export class CreateClientDto {}
+export class CreateClientDto {
+    systemId: number; 
+    name: string;
+    startDate: Date;
+    endDate: Date; 
+}
--- hyperlocal-server/src/client/entities/client.entity.ts
+++ hyperlocal-server/src/client/entities/client.entity.ts
@@ -1,1 +1,7 @@
-export class Client {}
+export class Client {
+    id: string; 
+    systemId: number; 
+    name: string;
+    startDate: Date;
+    endDate: Date; 
+}
