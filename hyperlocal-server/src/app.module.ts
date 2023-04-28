adef966130ee2b2e557afdc87f6bd894f637235e
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-28 17:12:33 +0100

    fix: bug fix wrong import

--- hyperlocal-server/src/app.module.ts
+++ hyperlocal-server/src/app.module.ts
@@ -3,7 +3,7 @@ import { DatabaseModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { TierModule } from './tier/tier.module';
-import { ClientModule } from './client/client.module';
+import { ClientModule } from './costumer/client.module';
 import { ProductsModule } from './product/products.module';
 
 @Module({
