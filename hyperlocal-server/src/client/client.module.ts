667aaf7ac59dfec62ed54248e336d46b2a0e3696
Matheus Mascarenhas <mathmasca@gmail.com>
2023-04-28 16:53:27 +0100

    feat. add auth

--- hyperlocal-server/src/client/client.module.ts
+++ hyperlocal-server/src/client/client.module.ts
@@ -3,9 +3,10 @@ import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
 import { DatabaseModule } from 'src/prisma/prisma.module';
 import { ClientRepository } from './client.repository';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports:[DatabaseModule],
+  imports:[DatabaseModule, PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [ClientController],
   providers: [ClientService, ClientRepository]
 })
